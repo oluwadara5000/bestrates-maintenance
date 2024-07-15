e0ebbd8aca94af73501888c48ba43e4d7cd3ad74
Oluwadarasimi Odejobi <oluwadarasimi5000@gmail.com>
2024-07-15 15:26:06 +0100

    done?

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
+import bgImg from '../public/construction-landing-page-template_52683-11358.jpg'
+
+
+
 export default function Home() {
   return (
-    <section className="py-24">
-      <div className="container">
-        <h1 className="text-3xl font-bold">Maintenance</h1>
+    <section className='flex py-32 4xl:py-52'>
+      <div className='container flex flex-col 4xl:flex-row justify-center gap-20'>
+        <div className='flex flex-col gap-5 4xl:gap-16'>
+          <h1 className='text-7xl 4xl:text-9xl font-black text-blue-700 3xl:text-8xl'>Oops...</h1>
+          <h1 className='text-3xl 4xl:text-5xl text-blue-700 3xl:text-4xl font-bold'>BestRates Digitals Is Under Construction</h1>
+          <h1 className='4xl:text-3xl text-blue-700 font-medium 3xl:text-3xl 2xl:text-3xl xl:text-3xl'>Try Again Later</h1>
+        </div>
+
+        <div className='flex items-center'>
+          <Image src={bgImg} alt='/' />
+        </div>
       </div>
     </section>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,20 +2,52 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     container: {
       center: true,
       padding: {
         DEFAULT: '1rem',
-        md: '1.5rem',
-        lg: '2rem'
+        md: '1rem',
+        lg: '1rem'
       }
+    },
+
+    screens: {
+      sm: '374px',
+      // => @media (min-width: 374px)
+
+      md: '450px',
+      // => @media (min-width: 450px)
+
+      lg: '525px',
+      // => @media (min-width: 525px)
+
+      xl: '600px',
+      // => @media (min-width: 600px)
+
+      '2xl': '685px',
+      // => @media (min-width: 685px)
+
+      '3xl': '768px',
+      // => @media (min-width: 768px)
+
+      '4xl': '990px',
+      // => @media (min-width: 990px)
+
+      '5xl': '1024px',
+      // => @media (min-width: 1024px)
+
+      '6xl': '1280px',
+      // => @media (min-width: 1280px)
+
+      '7xl': '1536px'
+      // => @media (min-width: 1536px)
     }
   },
-  plugins: [],
-};
+  plugins: []
+}
 export default config;
